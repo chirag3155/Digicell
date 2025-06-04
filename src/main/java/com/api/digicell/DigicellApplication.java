6dfa7c231f62e8c0e8fc521b55e36d28f9afb97b
Chirag Bhargava <chirag.bhargava09@gmail.com>
2025-06-04 18:36:46 +0530

    initial setup

---
+++ src/main/java/com/api/digicell/DigicellApplication.java
@@ -0,0 +1,13 @@
+package com.api.digicell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigicellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigicellApplication.class, args);
+	}
+
+}
