6c4ef88f69c6a1151fb1be2475529136947203f9
Chirag Bhargava <chirag.bhargava09@gmail.com>
2025-06-16 11:27:41 +0530

    cors appplied

--- src/main/java/com/api/digicell/DigicellApplication.java
+++ src/main/java/com/api/digicell/DigicellApplication.java
@@ -2,8 +2,12 @@ package com.api.digicell;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+import com.api.digicell.config.CorsConfig;
 
 @SpringBootApplication
+@Import(CorsConfig.class)
 public class DigicellApplication {
 
 	public static void main(String[] args) {
