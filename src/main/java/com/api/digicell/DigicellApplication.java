19909e66d255edfd5c104f9512894d3e60e19380
Chirag Bhargava <chirag.bhargava09@gmail.com>
2025-06-21 04:53:04 +0530

    elk repo removed

--- src/main/java/com/api/digicell/DigicellApplication.java
+++ src/main/java/com/api/digicell/DigicellApplication.java
@@ -3,11 +3,13 @@ package com.api.digicell;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import com.api.digicell.config.CorsConfig;
 
 @SpringBootApplication
 @Import(CorsConfig.class)
+@EnableJpaRepositories("com.api.digicell.repository")
 public class DigicellApplication {
 
 	public static void main(String[] args) {
